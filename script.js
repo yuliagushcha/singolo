c6398d1438c121c93e8cc699ffa8e88575c7d364
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-03-21 08:46:52 +0300

    feat: add animation to scrolling

--- script.js
+++ script.js
@@ -210,3 +210,20 @@ verticalThird.addEventListener('click', () => {
 blackDisplayThird.addEventListener('click', () => {
   blackDisplayThird.style.display = 'none';
 })
+
+// scrolling
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
